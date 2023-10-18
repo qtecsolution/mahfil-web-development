9fb14b687b669b17270988fc27163c86da634864
it27427 <istiak.tushar.qtec@gmail.com>
2023-10-18 13:57:56 +0600

    home-page completed

--- assets/js/home-slider/homeslider.js
+++ assets/js/home-slider/homeslider.js
@@ -1,17 +1,41 @@
-const homeSlider = new Swiper('.homeSlider', {
+const popularSlider = new Swiper('.popularSlider', {
   autoplay: {
     delay: 5000,
     disableOnInteraction: true,
   },
-  // loop: true,
-  speed: 500,
+  loop: true,
+  speed: 750,
   parallax: true,
-  spaceBetween: 40,
-  slidesPerView: 1,
-  centeredSlides: true,
+  spaceBetween: 60,
   pagination: false,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    576: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+    1600: {
+      slidesPerView: 6,
+    },
+    1800: {
+      slidesPerView: 7,
+    },
   },
 })

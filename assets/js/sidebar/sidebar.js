0126a3ff88f1f6848e59bdc87e1218436fca91f5
it27427 <istiak.tushar.qtec@gmail.com>
2023-10-18 12:01:24 +0600

    header-completed with responsive

---
+++ assets/js/sidebar/sidebar.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const brandLogo = document.querySelector('.brandlogo')
+  const iconLogo = document.querySelector('.iconlogo')
+  const sidebar = document.querySelector('.sidebar')
+  const linkTexts = document.querySelectorAll('.linktext')
+  const logTitle = document.querySelector('.logtitle')
+  const main = document.querySelector('.main')
+
+  const buttonHamburger = document.querySelector('.btn-hamburger')
+  const buttonShow = document.querySelector('.btn-show')
+
+  function collapseSidebar() {
+    buttonHamburger.classList.add('d-none')
+    brandLogo.classList.add('d-none')
+    sidebar.classList.add('sidecollapsed')
+    logTitle.classList.add('d-none')
+    main.classList.add('maincollapsed')
+    iconLogo.classList.remove('d-none')
+    buttonShow.classList.remove('d-none')
+
+    linkTexts.forEach((link) => {
+      link.classList.add('d-none')
+    })
+  }
+
+  function expandSidebar() {
+    buttonShow.classList.add('d-none')
+    brandLogo.classList.remove('d-none')
+    sidebar.classList.remove('sidecollapsed')
+    main.classList.remove('maincollapsed')
+    iconLogo.classList.add('d-none')
+    buttonHamburger.classList.remove('d-none')
+    logTitle.classList.remove('d-none')
+
+    linkTexts.forEach((link) => {
+      link.classList.remove('d-none')
+    })
+  }
+
+  buttonHamburger.addEventListener('click', collapseSidebar)
+  buttonShow.addEventListener('click', expandSidebar)
+})

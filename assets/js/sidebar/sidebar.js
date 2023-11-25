f2a0dfa8248477da743cdb2c072c584025195a2c
it27427 <istiak.tushar.it@gmail.com>
2023-11-25 11:41:55 +0600

    header position changed

--- assets/js/sidebar/sidebar.js
+++ assets/js/sidebar/sidebar.js
@@ -1,39 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const brandLogo = document.querySelector('.logo')
-  const sidebar = document.querySelector('.sidebar')
-  const linkTexts = document.querySelectorAll('.linktext')
-  const logTitle = document.querySelector('.logtitle')
-  const main = document.querySelector('.main')
+document.addEventListener("DOMContentLoaded", () => {
+  const brandLogo = document.querySelector(".logo");
+  const sidebar = document.querySelector(".sidebar");
+  const linkTexts = document.querySelectorAll(".linktext");
+  const logTitle = document.querySelector(".logtitle");
+  const header = document.querySelector(".header");
+  const main = document.querySelector(".main");
 
-  const buttonHamburger = document.querySelector('.btn-hamburger')
-  const buttonShow = document.querySelector('.btn-show')
+  const buttonHamburger = document.querySelector(".btn-hamburger");
+  const buttonShow = document.querySelector(".btn-show");
 
   function collapseSidebar() {
-    buttonHamburger.classList.add('d-none')
-    buttonShow.classList.remove('d-none')
-    brandLogo.classList.add('d-none')
-    sidebar.classList.add('sidecollapsed')
-    logTitle.classList.add('d-none')
-    main.classList.add('maincollapsed')
+    buttonHamburger.classList.add("d-none");
+    buttonShow.classList.remove("d-none");
+    brandLogo.classList.add("d-none");
+    sidebar.classList.add("sidecollapsed");
+    logTitle.classList.add("d-none");
+    header.classList.add("headercollapsed");
+    main.classList.add("maincollapsed");
 
     linkTexts.forEach((link) => {
-      link.classList.add('d-none')
-    })
+      link.classList.add("d-none");
+    });
   }
 
   function expandSidebar() {
-    buttonShow.classList.add('d-none')
-    brandLogo.classList.remove('d-none')
-    sidebar.classList.remove('sidecollapsed')
-    main.classList.remove('maincollapsed')
-    buttonHamburger.classList.remove('d-none')
-    logTitle.classList.remove('d-none')
+    buttonShow.classList.add("d-none");
+    brandLogo.classList.remove("d-none");
+    sidebar.classList.remove("sidecollapsed");
+    header.classList.remove("headercollapsed");
+    main.classList.remove("maincollapsed");
+    buttonHamburger.classList.remove("d-none");
+    logTitle.classList.remove("d-none");
 
     linkTexts.forEach((link) => {
-      link.classList.remove('d-none')
-    })
+      link.classList.remove("d-none");
+    });
   }
 
-  buttonHamburger.addEventListener('click', collapseSidebar)
-  buttonShow.addEventListener('click', expandSidebar)
-})
+  buttonHamburger.addEventListener("click", collapseSidebar);
+  buttonShow.addEventListener("click", expandSidebar);
+});
